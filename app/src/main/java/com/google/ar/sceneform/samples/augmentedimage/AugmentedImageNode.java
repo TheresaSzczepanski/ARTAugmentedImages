eff73cf7a8f98719b9eea84cb8110374d03fd4d4
robertbrodin <43351978+robertbrodin@users.noreply.github.com>
2019-01-18 14:48:17 -0500

    augmentedImageNode now places a video on detection

--- app/src/main/java/com/google/ar/sceneform/samples/augmentedimage/AugmentedImageNode.java
+++ app/src/main/java/com/google/ar/sceneform/samples/augmentedimage/AugmentedImageNode.java
@@ -17,14 +17,46 @@
 package com.google.ar.sceneform.samples.augmentedimage;
 
 import android.content.Context;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.util.Log;
 import com.google.ar.core.AugmentedImage;
 import com.google.ar.sceneform.AnchorNode;
 import com.google.ar.sceneform.Node;
+import com.google.ar.sceneform.lullmodel.ModelPipelineSkeletonDef;
+import com.google.ar.sceneform.math.Quaternion;
 import com.google.ar.sceneform.math.Vector3;
 import com.google.ar.sceneform.rendering.ModelRenderable;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+import com.google.ar.sceneform.rendering.Color;
+import com.google.ar.sceneform.rendering.ExternalTexture;
+import android.graphics.SurfaceTexture;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.graphics.SurfaceTexture;
+import android.media.MediaPlayer;
+import android.os.Build;
+import android.os.Build.VERSION_CODES;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.widget.Toast;
+import com.google.ar.core.Anchor;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.Node;
+import com.google.ar.sceneform.math.Vector3;
+import com.google.ar.sceneform.rendering.Color;
+import com.google.ar.sceneform.rendering.ExternalTexture;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.ux.ArFragment;
+
 
 /**
  * Node for rendering an augmented image. The image is framed by placing the virtual picture frame
@@ -46,6 +78,17 @@ public class AugmentedImageNode extends AnchorNode {
   private static CompletableFuture<ModelRenderable> lrCorner;
   private static CompletableFuture<ModelRenderable> llCorner;
 
+  // The color to filter out of the video.
+  private static final Color CHROMA_KEY_COLOR = new Color(0.1843f, 1.0f, 0.098f);
+
+  // Controls the height of the video in world space.
+  private static final float VIDEO_HEIGHT_METERS = 0.2f;
+
+  private CompletableFuture<ModelRenderable> videoRenderable;
+  @Nullable private ModelRenderable videoPlacedRenderable;
+
+  private MediaPlayer mediaPlayer;
+
   public AugmentedImageNode(Context context) {
     // Upon construction, start loading the models for the corners of the frame.
     if (ulCorner == null) {
@@ -66,6 +109,12 @@ public class AugmentedImageNode extends AnchorNode {
               .setSource(context, Uri.parse("models/frame_upper_right.sfb"))
               .build();
     }
+    if(videoRenderable == null){
+      videoRenderable =
+              ModelRenderable.builder()
+                      .setSource(context, Uri.parse("models/chroma_key_video.sfb"))
+                      .build();
+    }
   }
 
   /**
@@ -75,23 +124,92 @@ public class AugmentedImageNode extends AnchorNode {
    * relative to the center of the image, which is the parent node of the corners.
    */
   @SuppressWarnings({"AndroidApiChecker", "FutureReturnValueIgnored"})
-  public void setImage(AugmentedImage image) {
+
+  // TODO: Adding video to image. Working on it right now.
+  // TODO: Changing context.
+  public void setImage(AugmentedImage image, Context context) {
     this.image = image;
+    ExternalTexture texture = new ExternalTexture();
+
 
     // If any of the models are not loaded, then recurse when all are loaded.
     if (!ulCorner.isDone() || !urCorner.isDone() || !llCorner.isDone() || !lrCorner.isDone()) {
       CompletableFuture.allOf(ulCorner, urCorner, llCorner, lrCorner)
-          .thenAccept((Void aVoid) -> setImage(image))
+          .thenAccept((Void aVoid) -> setImage(image, context))
           .exceptionally(
               throwable -> {
                 Log.e(TAG, "Exception loading", throwable);
                 return null;
               });
+
+      // TODO: This will probably not work.
+      CompletableFuture.allOf(videoRenderable)
+              .handle((notUsed, throwable) -> {
+                // When you build a Renderable, Sceneform loads its resources in the background while
+                // returning a CompletableFuture. Call handle(), thenAccept(), or check isDone()
+                // before calling get().
+
+                if (throwable != null) {
+                  return null;
+                }
+
+                try {
+                  videoPlacedRenderable = videoRenderable.get();
+                  videoPlacedRenderable.getMaterial().setExternalTexture("videoTexture", texture);
+                  videoPlacedRenderable.getMaterial().setFloat4("keyColor", CHROMA_KEY_COLOR);
+
+                  // Everything finished loading successfully.
+                } catch (InterruptedException | ExecutionException ex) {
+                }
+
+                return null;
+                      });
     }
+
+  // TODO: MEDIAPLAYER NOT WORKING. In the process of switching code over.
+
+    mediaPlayer = MediaPlayer.create(context, R.raw.shaq);
+    mediaPlayer.setSurface(texture.getSurface());
+    mediaPlayer.setLooping(true);
 
     // Set the anchor based on the center of the image.
     setAnchor(image.createAnchor(image.getCenterPose()));
 
+
+    //TODO: Video nodes here.
+    Node videoNode = new Node();
+    videoNode.setParent(this);
+
+
+    float videoWidth = mediaPlayer.getVideoWidth();
+    float videoHeight = mediaPlayer.getVideoHeight();
+    videoNode.setLocalScale(
+            new Vector3(
+                    VIDEO_HEIGHT_METERS * (videoWidth / videoHeight), VIDEO_HEIGHT_METERS, 1.0f));
+
+    Quaternion q1 = videoNode.getLocalRotation();
+    Quaternion q2 = Quaternion.axisAngle(new Vector3(1.0f, 0f, 0f), 90f);
+    videoNode.setLocalRotation(Quaternion.multiply(q1, q2));
+
+    if (!mediaPlayer.isPlaying()) {
+      mediaPlayer.start();
+
+      // Wait to set the renderable until the first frame of the  video becomes available.
+      // This prevents the renderable from briefly appearing as a black quad before the video
+      // plays.
+      texture
+              .getSurfaceTexture()
+              .setOnFrameAvailableListener(
+                      (SurfaceTexture surfaceTexture) -> {
+                        videoNode.setRenderable(videoPlacedRenderable);
+                        texture.getSurfaceTexture().setOnFrameAvailableListener(null);
+                      });
+    } else {
+      videoNode.setRenderable(videoPlacedRenderable);
+    }
+
+
+    /*
     // Make the 4 corner nodes.
     Vector3 localPosition = new Vector3();
     Node cornerNode;
@@ -117,6 +235,8 @@ public class AugmentedImageNode extends AnchorNode {
     then focus on changing the jpeg
      */
 
+
+    /*
     // Lower right corner.
     localPosition.set(0.5f * image.getExtentX(), 0.0f, 0.5f * image.getExtentZ());
     cornerNode = new Node();
@@ -130,6 +250,7 @@ public class AugmentedImageNode extends AnchorNode {
     cornerNode.setParent(this);
     cornerNode.setLocalPosition(localPosition);
     cornerNode.setRenderable(llCorner.getNow(null));
+    */
   }
 
   public AugmentedImage getImage() {
