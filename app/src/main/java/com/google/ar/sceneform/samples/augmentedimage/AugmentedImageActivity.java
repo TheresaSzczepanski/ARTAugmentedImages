fbb78120cbfca065ea72df67cd442bbf66fbcbf2
robertbrodin <43351978+robertbrodin@users.noreply.github.com>
2019-02-18 08:12:35 -0500

    Added switching between audio and video
    In particular, I updated what I programmed yesterday and copied our prior code (for playing audio) into AugmentedImageActivity. Arguably, the most important feature that was changed is the location of where audio is created and started, which is now under the hashmap check (to see if an image has been detected before). I created a new list of booleans (in AugmentedImageFragment, similar to videoList and musicList) to check if a video should be played or music should be played. The program checks if a video should be played first (AugmentedImageFragment.imagePlaysVideoBooleanList[augmentedImage.getIndex() is true), otherwise, an audio file is played.

--- app/src/main/java/com/google/ar/sceneform/samples/augmentedimage/AugmentedImageActivity.java
+++ app/src/main/java/com/google/ar/sceneform/samples/augmentedimage/AugmentedImageActivity.java
@@ -53,21 +53,28 @@ import android.media.MediaPlayer;
 Programmed by Rising Tide Augmented Reality Team (ART) - Joseph Turner, Emma Riley, Nora Rooney, and Robert Brodin - 2019
 
 AugmentedImage - Video version, detects images and places specific videos (see AugmentedImageFragment). When a different image is detected, the video playing is stopped and a new video is started (specific to the new image).
+Also works with playing audio files instead of video files (see imagePlaysBooleanList in AugmentedImageFragment)
 
  */
 
 public class AugmentedImageActivity extends AppCompatActivity {
-  
+
+
+  // Used to keep track of which video is currently playing (is changed ONLY in AugmentedImageNode)
   public static Integer augmentedImageVideoPlayerIndex = null;
+  // mediaAugmentedImage is used to check if an image has been detected before, and is also used to remove specific keys from the augmented image hashmap. TODO: This will absolutely have to be changed for placing renderables and keeping renderables placed. This makes sense for videos and music, but not for renderables (ARTGallery)
   private AugmentedImage mediaAugmentedImage = null;
+  // Both videoRenderable and videoPlacedRenderable are used by AugmentedImageNode, and are declared in this class to only require them to be rendered/loaded once.
   public static CompletableFuture<ModelRenderable> videoRenderable;
   @Nullable public static ModelRenderable videoPlacedRenderable;
 
+  // audioMediaPlayer and currentSongIndex are used to play audio files (generally from musicList in AugmentedImageFragment)
+  private MediaPlayer audioMediaPlayer;
+  private Integer currentSongIndex = null;
 
+  // Required ARCore ArFragment and ImageView.
   private ArFragment arFragment;
   private ImageView fitToScanView;
-
-  //private AugmentedImageNode node;
 
   // Augmented image and its associated center pose anchor, keyed by the augmented image in
   // the database.
@@ -99,69 +106,92 @@ public class AugmentedImageActivity extends AppCompatActivity {
    */
   private void onUpdateFrame(FrameTime frameTime) {
     Frame frame = arFragment.getArSceneView().getArFrame();
-
     // If there is no frame or ARCore is not tracking yet, just return.
     if (frame == null || frame.getCamera().getTrackingState() != TrackingState.TRACKING) {
       return;
     }
 
-
     Collection<AugmentedImage> updatedAugmentedImages =
         frame.getUpdatedTrackables(AugmentedImage.class);
+    // Loops through all of the images in the database (see imageList in AugmentedImageFragment )
     for (AugmentedImage augmentedImage : updatedAugmentedImages) {
-
       switch (augmentedImage.getTrackingState()) {
         case PAUSED:
           // When an image is in PAUSED state, but the camera is not PAUSED, it has been detected,
           // but not yet tracked.
           String text = "Detected Image " + augmentedImage.getIndex();
           SnackbarHelper.getInstance().showMessage(this, text);
-
           break;
-
+          // If an image is detected
         case TRACKING:
-
           // Have to switch to UI Thread to update View.
           fitToScanView.setVisibility(View.GONE);
-
 
           // Checks if the node that is used for the video is not null, if the node's mediaplayer is not null, and if the current image detected is not the same image as the last image detected.
           // This section is used to stop the video from playing.
           if((AugmentedImageStaticNode.node != null) &&(!(AugmentedImageStaticNode.node.nodeMediaPlayer == null)) && augmentedImage.getIndex() != augmentedImageVideoPlayerIndex) {
-
-
             // Video is stopped.
             AugmentedImageStaticNode.node.stopVideo();
             // Node that the video is placed on is removed from the scene.
             AugmentedImageStaticNode.node.removeChild(AugmentedImageStaticNode.node.videoNode);
             // the image that correlates to the video that is being stopped is removed from the hashmap of detected images (makes it so if that same image is detected later, a video will be played)
             augmentedImageMap.remove(mediaAugmentedImage);
-
           }
-
+          // If the augmentedImageMap (HASHMAP) has not seen the image before (the augmentedImage key is not in the hashmap), then a video or audio file is played.
           if (!augmentedImageMap.containsKey(augmentedImage)) {
 
             // New AugmentedImageNode class is created in THIS context.
             AugmentedImageStaticNode.node = new AugmentedImageNode(this);
-            // startVideo takes arguments: context, imageIndex, and texture. This method starts the video (using .create()), and places it on the surface.
-            AugmentedImageStaticNode.node.startVideo(this, augmentedImage.getIndex(), AugmentedImageStaticNode.node.texture);
-            // Set image places the video on a renderable.
-            AugmentedImageStaticNode.node.setImage(this, augmentedImage, augmentedImage.getIndex());
-            // augmentedImage is put in a hashmap, will be used to detect if the image has already been detected (deprecated in this version)
-            augmentedImageMap.put(augmentedImage, AugmentedImageStaticNode.node);
-            arFragment.getArSceneView().getScene().addChild(AugmentedImageStaticNode.node);
-            mediaAugmentedImage = augmentedImage; // fixes the issue of videos not playing with their specific image if that image has been detected before.
 
+            // Used to check if a video will be played or if an audio file will be played. This, when true, creates a video.
+            if(AugmentedImageFragment.imagePlaysVideoBooleanList[augmentedImage.getIndex()]) {
+              // In the event that there is already music playing, the mediaplayer is stopped and released.
+              if(audioMediaPlayer.isPlaying()) {
+                audioMediaPlayer.stop();
+                audioMediaPlayer.release();
+                currentSongIndex = null;
+              }
+              // startVideo takes arguments: context, imageIndex, and texture. This method starts the video (using .create()), and places it on the surface.
+              AugmentedImageStaticNode.node.startVideo(this, augmentedImage.getIndex(), AugmentedImageStaticNode.node.texture);
+              // Set image places the video on a renderable.
+              AugmentedImageStaticNode.node.setImage(this, augmentedImage, augmentedImage.getIndex());
+              // augmentedImage is put in a hashmap, will be used to detect if the image has already been detected (deprecated in this version)
+              augmentedImageMap.put(augmentedImage, AugmentedImageStaticNode.node);
+              arFragment.getArSceneView().getScene().addChild(AugmentedImageStaticNode.node);
+              // Fixes the issue of videos not playing with their specific image if that image has been detected before.
+              mediaAugmentedImage = augmentedImage;
+            }
+            // Otherwise, an audio file is created and started.
+            else{
+              if (currentSongIndex == null) {
+                // If there is no song playing, set currentSongIndex to the song that aligns with
+                // the detected image
+                currentSongIndex = augmentedImage.getIndex();
+                // Creates a newAugmentedImageStaticNode.node object with the correct song
+                // Start the song
+                audioMediaPlayer = MediaPlayer.create(this, AugmentedImageFragment.Music_list[currentSongIndex]);
+                audioMediaPlayer.start();
 
+              }
+              else if((currentSongIndex != augmentedImage.getIndex())){
+                // If the song that is playing does not match up with the song the image it detects
+                // is assigned to:
+                // Assign the correct song to currentSongIndex
+                // Stop playing the song, reset theAugmentedImageStaticNode.node object
+                currentSongIndex = augmentedImage.getIndex();
+                audioMediaPlayer.stop();
+                audioMediaPlayer.release();
+                // Recreate theAugmentedImageStaticNode.node object with the correct song
+                // Start the song
+                audioMediaPlayer = MediaPlayer.create(this, AugmentedImageFragment.Music_list[currentSongIndex]);
+                audioMediaPlayer.start();
+              }
+            }
           }
           break;
-
         case STOPPED:
-
           augmentedImageMap.remove(augmentedImage);
-
           break;
-
         default:
 
       }
